5fa52aa58d6a56dbbcfc5b25c00973cf950445b4
Hendrawahyudii <32281589+Hendrawahyudii@users.noreply.github.com>
2018-01-19 21:13:11 +0800

    wow kita segera memulainya

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -10,3 +10,14 @@ class motor {
         void jumlahBan(int jumlah);
         void ukuranMotor(string ganalkahhalus);
         };
+        int main(){
+	motor YamahaIngkal,Honda70,YamahaMio,HondaSupra,KawasakiNinja,HarliDavidson,SapidaMotorRossi;
+	YamahaIngkal.cepat		= "Laki jadul";
+	Honda70.cepat			= "Bebek jadul";
+	YamahaMio.cepat			= "Matik ";
+	HondaSupra.cepat 		= "Bebek";
+	KawasakiNinja.cepat		= "Laki";
+	HarliDavidson.cepat		= "Laki";
+	SapidaMotorRossi.cepat	= "Laki Sport";
+	}
+	
