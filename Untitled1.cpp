4fb1302086356a7e270bf980b670a7216cbc5c75
Hendrawahyudii <32281589+Hendrawahyudii@users.noreply.github.com>
2018-01-19 21:09:01 +0800

    perjalanan kita di mulai

---
+++ Untitled1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class motor {
+    public:
+        void suara(string bunyi);
+        int jalan(string roda);
+        string cepat;
+        void jumlahBan(int jumlah);
+        void ukuranMotor(string ganalkahhalus);
+        };
